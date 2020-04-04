c5a07ef1afee3da80756aa9957c3aba604f2c4fa
jeldikk <jeldi.kamal2013@gmail.com>
2020-04-04 15:13:49 +0530

    added assets folder with necessary css and js file

---
+++ assets/scripts/main.js
@@ -0,0 +1,34 @@
+// $(function(){
+//     $(".header").backstretch("/assets/images/header_background_y2.JPG");
+// });
+$(function(){
+    $(".header").height($(window).height());
+
+    $(window).on('resize',function(){
+        //console.log("resize event invoked");
+        //console.log(`New height ${$(window).height()}`);
+        //console.log(`New width ${$(window).width()}`);
+        $(".header").height($(window).height());
+    });
+
+    AOS.init();
+
+    $("#aboutCarousel").on("slide.bs.carousel",function(event){
+        //console.log("slide instance method invoked");
+        //console.log(event);
+        let to_node = `#about-${event.to}`;
+        let from_node = `#about-${event.from}`;
+        $(from_node).removeClass("aboutme_active_nav");
+        $(to_node).addClass("aboutme_active_nav");
+
+    });
+
+    $(".navbar-nav > li").on('click',function(e){
+        //console.log("navbar link clicked");
+        $(".navbar-collapse").collapse('hide');
+    })
+
+    // $("#aboutCarousel").on("slid.bs.carousel",function(event){
+    //     console.log(event);
+    // })
+})
